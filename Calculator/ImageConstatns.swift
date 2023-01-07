1ab708cbbc4d61d2783a52d0471d7c41f0be9623
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-07 15:08:11 +0100

    first commit and setup colors assets

---
+++ Calculator/ImageConstatns.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+let buttonBackground = "buttonBackground"
+let foregroundDigits = "foregroundDigits"
+let foregroundRightButtons = "foregroundRightButtons"
+let foregroundTopButtons = "foregroundTopButtons"
+let primaryBackground = "primaryBackground"
+let secondaryBackground = "secondaryBackground"
